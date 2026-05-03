a16607d368bc3f49af2819fcd4e229355e8bf564
GroteGitJeppo <j.m.spuesens@tilburguniversity.edu>
2026-05-03 18:06:33 +0200

    encoder update

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .encoder import PointNetEncoder
+from .encoder_old import PointNetEncoder as PointNetEncoderOld
 from .decoder import SDFDecoder
 from .pointsdf import PointSDF
 
-__all__ = ['PointNetEncoder', 'SDFDecoder', 'PointSDF']
+__all__ = ['PointNetEncoder', 'PointNetEncoderOld', 'SDFDecoder', 'PointSDF']
